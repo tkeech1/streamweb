07c9341dae02dd402bddf6daea98fc1b4cfde217
Todd Keech <tkeech1@hotmail.com>
2021-06-22 20:04:18 -0400

    changed nosb layout

--- streamweb/layout_clean_no_sidebar.py
+++ streamweb/layout_clean_no_sidebar.py
@@ -1,97 +1,59 @@
 import streamlit as st
-import sys
-from typing import List
-from types import ModuleType
+import logging
 import site_config
-
-from utils.siteutils import (
-    load_content,
-    render_content_by_click,
-    render_content_by_key,
-)
-
-
-def render_home_content(content: List[ModuleType]) -> None:
-    title_col1, title_col2 = st.beta_columns([3, 2])
-    title_col1.header("Posts")
-
-    search_text = title_col2.text_input("Search Posts", "")
-
-    for module in content:
-        display = True
-        if search_text:
-            if search_text.lower() not in module.long_title.lower():
-                display = False
-        if display:
-            expander = st.beta_expander(module.long_title)
-            with expander:
-                module.render()
-
-    st.subheader("")
-    st.markdown("[RSS](feeds/dynamic_rss.xml) | [Atom](feeds/dynamic_atom.xml)")
-
-
-recent_dynamic_content_list_length = 3
+from utils.siteutils import StreamwebSite
 
 st.set_page_config(layout="centered")
 
-environment = None
-if len(sys.argv) > 1:
-    environment = sys.argv[1]
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-content_id = None
+
+def main(sws: StreamwebSite, content_id: str):
+
+    sws.load_content("static")
+    sws.load_content("dynamic")
+
+    st.markdown(
+        f"# <a href='/' target='_self'>{sws.website_title}</a>",
+        unsafe_allow_html=True,
+    )
+
+    if content_id:
+        # the url contains a content_id (bookmark or direct link to page)
+        sws.render_content_by_key("dynamic", location=st, content_key=content_id)
+    else:
+        # the user browsed to the home page (didn't click the home button)
+        st.experimental_set_query_params()
+        sws.create_link_list(content_name="dynamic", location=st, item_label="Posts")
+
+    # workaround to hide hamburger menu, header and footer
+    # https://github.com/streamlit/streamlit/issues/395
+    hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+    # hide footer
+    # footer {visibility: hidden;}
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+    # end workaround to hide hamburger menu
+
+
+sws: StreamwebSite = StreamwebSite(
+    site_config.website_id,
+    site_config.website_title,
+    site_config.website_description,
+    site_config.website_author,
+    site_config.website_url,
+    site_config.website_language,
+    site_config.environment,
+)
+
+content_id = ""
 query_params = st.experimental_get_query_params()
 if "content" in query_params:
     content_id = query_params["content"][0]
 
-st.title(site_config.website_title)
-
-static_content = load_content("static", environment)
-
-static_content_button_click = []
-cols = len(static_content) + 1
-if cols < 6:
-    cols = max(len(static_content), 6)
-
-# this is a workaround since it doesn't appear possible to
-# get the key of the button that was clicked
-# https://discuss.streamlit.io/t/how-to-use-the-key-field-in-interactive-widgets-api/1007
-[*button_columns] = st.beta_columns(cols)
-home_button = button_columns[0].button("home")
-for content, button_column in zip(static_content, button_columns[1:]):
-    static_content_button_click.append(
-        button_column.button(content.short_title, key=content.key)
-    )
-
-dynamic_content = load_content("dynamic", environment, feed=True)
-
-if home_button:
-    st.experimental_set_query_params()
-    render_home_content(content=dynamic_content)
-elif any(static_content_button_click):
-    st.experimental_set_query_params()
-    render_content_by_click(
-        content=static_content,
-        button_click=static_content_button_click,
-    )
-elif content_id:
-    render_content_by_key(
-        content=dynamic_content,
-        content_key=content_id,
-    )
-else:
-    st.experimental_set_query_params()
-    render_home_content(content=dynamic_content)
-
-# workaround to hide hamburger menu, header and footer
-# https://github.com/streamlit/streamlit/issues/395
-hide_menu_style = """
-       <style>
-       #MainMenu {visibility: hidden;}
-       header {visibility: hidden;}
-       </style>
-       """
-# hide footer
-# footer {visibility: hidden;}
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-# end workaround to hide hamburger menu
+main(sws, content_id)
