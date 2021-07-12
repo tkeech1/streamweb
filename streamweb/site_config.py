6378df113dbf9c0d91132efbba983e314a0fe81c
Todd Keech <tkeech1@hotmail.com>
2021-07-11 20:57:22 -0400

    trailing slash on url

--- streamweb/site_config.py
+++ streamweb/site_config.py
@@ -1,8 +1,8 @@
 from typing import Dict
 
-website_id: str = "http://toddkeech.com"
+website_id: str = "http://toddkeech.com/"
 website_title: str = "toddkeech.com"
-website_url: str = "http://toddkeech.com"
+website_url: str = "http://toddkeech.com/"
 website_description: str = "Notes."
 website_author: Dict[str, str] = {"name": "Todd Keech", "email": "tk@toddkeech.com"}
 website_language: str = "en"
