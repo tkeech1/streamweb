dab8a27aa8d3775a0e9b81fcc131c866acddab91
Todd Keech <tkeech1@hotmail.com>
2021-10-06 19:31:48 -0400

    itertuples post

--- streamweb/dynamic/iterrows_itertuples.py
+++ streamweb/dynamic/iterrows_itertuples.py
@@ -17,13 +17,15 @@ def render(location: st):
     location.markdown(f"## [{long_title}](/?content={key})")
     location.write(f"*{content_date.strftime('%m.%d.%Y')}*")
 
-    location.write(f"Pandas `iterrows()` and `itertuples()` provide two different ways to `iter`ate over rows in a DataFrame.")
+    location.write(f"Pandas `iterrows()` and `itertuples()` provide two different ways to `iter`ate over rows in a DataFrame. "
+    "If you're working with a data set that contains columns with different data "
+    "types, use caution since `iterrows()` and `itertuples()` may not always return the data you expect. Here's an example based on the "
+    "Pandas documentation."
+    )
 
     location.write(f"### iterrows()")
 
-    location.write(f"`iterrows()` returns a Pandas `Series`. If you're working with a data set that contains columns with different data "
-    "types, `iterrows()` might not be what you want since it doesn't preserve data types of each column. Here's an example based on the "
-    "Pandas documentation."
+    location.write(f"`iterrows()` returns a Pandas `Series`. "
     )
 
     # this works
@@ -32,8 +34,8 @@ def render(location: st):
     # ser = pd.Series(['1','2','3.0'])
     # location.write(ser)
     # this fails - seems like a streamlit bug
-    # ser = pd.Series(['1', 2, 3])
-    # location.write(ser)
+    #ser = pd.Series(['1', 2, 3])
+    #location.write(ser)
     # this also fails
     # ser = pd.Series([1, '2', 3])
     # location.write(ser)
@@ -95,7 +97,7 @@ type(row.date)
         """
     )  
     location.write(type(row.date))     
-    location.write('Returning the date from the tuple works as expected. But returning the category...')
+    location.write('Returning the date from the tuple works as expected. But returning the category column doesn''t return a category object. It returns an `int` in this case. ')
 
     location.code(
         """
@@ -103,15 +105,24 @@ type(row.cat)
         """
     )  
     location.write(type(row.cat))   
-    #location.write('Wait... what?!')
 
-    location.markdown("### Performance")
+    location.markdown("### Performance")    
 
-    df = pd.DataFrame([i for i in range(1000)])
+    location.markdown("`iterrows()` and `itertuples()` might be useful if you must iterate over a Pandas dataframe. But from a performance perspective, both `iterrows()` and `itertuples()` should be avoided in favor of "
+    "code that uses vectorization. Vectorization allows the CPU to work on multiple values at a time and it's what makes numpy operations blazingly fast. "
+    "If you must iterate, you'll probably find `itertuples()` to be significantly faster. You should test your code with `timeit` or another performance testing tool to make sure `itertuples()` is actually faster.")
 
-    #location.write(df)
+    location.code("""
+df = pd.DataFrame([i for i in range(10000)]) # create a dataframe with 1000 rows    
+    """)
+    
+    df = pd.DataFrame([i for i in range(10000)])
+
+    location.code("""
+for i in df.iterrows(): pass
+    """)
+
     s = 0
-
     x = timeit.repeat(
             stmt='for i in df.iterrows(): pass',
             repeat=1,
@@ -119,7 +130,7 @@ type(row.cat)
             globals=dict(globals(), **locals()),
         )
 
-    location.write(x)
+    location.markdown(f"Runtime: {x[0]} s")
 
     x = timeit.repeat(
             stmt='for i in df.itertuples(): pass',
@@ -128,25 +139,40 @@ type(row.cat)
             globals=dict(globals(), **locals()),
         )
 
-    location.write(x)
+    location.code("""
+for i in df.itertuples(): pass
+    """)
+
+    location.markdown(f"Runtime: {x[0]} s")
+
+    location.markdown("Interestingly, `apply()` seems to be slower than `itertuples()` in some cases such as `apply`ing the function to every row (`axis=1`). "
+    "For a long time, I assumed `apply()` was always faster but that's not the case in every situation.")
 
     x = timeit.repeat(
-            stmt='df.sum()',
+            stmt='df.apply(lambda row: None, axis=1)',
             repeat=1,
             number=1,
             globals=dict(globals(), **locals()),
         )
 
-    location.write(x)
+    location.code("""
+df.apply(lambda row: None, axis=1)
+""")
 
-    x = timeit.repeat(
-            stmt='df.apply(lambda row: 1==1, axis=1)',
-            repeat=1,
-            number=1,
-            globals=dict(globals(), **locals()),
-        )
+    location.markdown(f"Runtime: {x[0]} s")
 
-    location.write(x)
+    location.markdown("Thanks for reading.")
+
+    location.markdown("### Resources")
+
+    location.write(
+        """
+    * [iterrows() Documentation](https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.iterrows.html)
+    * [itertuples() Documentation](https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.itertuples.html)
+    * [Pandas Basics](https://pandas.pydata.org/pandas-docs/stable/user_guide/basics.html?highlight=vectorize#iteration)
+    
+"""
+    )
 
 
 
